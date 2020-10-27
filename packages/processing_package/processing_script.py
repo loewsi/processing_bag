8ee7ccfb272dcac584ac61a2bdf38513876a301a
loewsi <loewsi@student.ethz.ch>
2020-10-27 16:26:47 +0100

    first try

--- packages/processing_package/processing_script.py
+++ packages/processing_package/processing_script.py
@@ -1,8 +1,17 @@
 import rosbag
-import numpy as np
-bag = rosbag.Bag('/bags/amod20-rh3-ex-record-silvan-loew.bag')
-readtopics = list(bag.get_type_and_topic_info()[1].keys())
+import cv2
+from cv_bridge import CvBridge
+
+inbag = rosbag.Bag('amod20-rh3-ex-record-silvan-loew.bag')
+outbag= rosbag.BAG('outbag.bag', 'w')
+br = CvBridge()
 
 
-for topic, msg, t in bag.read_messages(topics=str(readtopics[i])):
-    print(t)
+for topic, msg, t in inbag.read_messages(topics = '/vonduck/camera_node/image/compressed'):
+    im = br.imgmsg_to_cv2(msg)
+    im2 = cv2.putText(im, t, (10, 10), FONT_HERSHEY_SIMPLEX, 1, (0,0,0))
+    msg2 = br.cvt_to_imgmsg(im2)
+    outbag.write('/vonduck/camera_node/image/compressed', msg2)
+
+inbag.close()
+outbag.close()
